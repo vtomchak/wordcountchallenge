e46a805fc56bf3b51f5578850059b8f22eba0b92
Veronica Tomchak <vtomchak27@gmail.com>
2021-06-19 11:03:56 -0400

    started design layout andfixed babel compatibility issue on local machine

---
+++ src/TextInput.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const TextInput = () => <div>Hi</div>;
+
+export default TextInput;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import TextInput from './TextInput';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = function () {
+  return (
+    <div class='ui raised very padded text container segment'>
+      <form class='ui form'>
+        <div class='ui huge header'>Word Counter</div>
+        <TextInput />
+        <div class='field'>
+          <textarea placeholder='Write or copy your text here...'></textarea>
+        </div>
+        <div class='field'>
+          <div class='ui checkbox'>
+            <input type='checkbox' tabindex='0' />
+            <label>Option 1</label>
+          </div>
+        </div>
+        <button class='ui right button' type='submit'>
+          Submit
+        </button>
+        <button class='ui left button' type='submit'>
+          Clear Text
+        </button>
+      </form>
+    </div>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.querySelector('#root'));
