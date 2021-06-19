bc7b16aa39409b381093ef08aa2b24060de41370
Veronica Tomchak <vtomchak27@gmail.com>
2021-06-19 11:20:22 -0400

    debug

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TextInput from './TextInput';
 
 const App = function () {
   return (
     <div class='ui raised very padded text container segment'>
       <form class='ui form'>
         <div class='ui huge header'>Word Counter</div>
-        <TextInput />
         <div class='field'>
           <textarea placeholder='Write or copy your text here...'></textarea>
         </div>
